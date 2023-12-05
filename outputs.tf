357f814b8101c0ce075e5ca5f316232174d2142d
Matthias Antierens <matthias.antierens@nipro-group.com>
2023-12-05 16:18:59 +0100

    add hostpool ID to outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "network_interfaces" {
   description = "An array of the NIC-objects created by this module."
   value       = azurerm_network_interface.main
 }
+
+output "hostpool_id" {
+  description = "The ID of the hostpool."
+  value       = azurerm_virtual_desktop_host_pool.main.id
+}
